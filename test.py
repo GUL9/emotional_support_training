50886237373e1269bb2973de92009d6795c1dd4e
Anton <a_widmark@hotmail.com>
2020-12-14 13:38:47 +0100

    fix

--- test.py
+++ test.py
@@ -3,6 +3,13 @@ from owlready2 import *
 onto = get_ontology("ontologies/ontology.owl").load()
 
 with onto:
+    # Patient setup
     patient = onto.Patient()
+    facial_expression = onto.Emotion()
+
+    patient.has_facial_expression = [onto.Emotion()]
     user = onto.User()
+
+
+
 
