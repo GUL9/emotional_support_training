b21f504560806d6feae7fe2d70ffca634712fa9e
Anton <a_widmark@hotmail.com>
2020-12-14 13:17:14 +0100

    added new ontolgy

--- test.py
+++ test.py
@@ -1,27 +1,8 @@
 import owlready2
-owlready2.JAVA_EXE = "C:\Coding\programs\java\bin"
 from owlready2 import *
-onto = get_ontology("ontology.owl").load()
-
-
-
+onto = get_ontology("ontologies/ontology.owl").load()
 
 with onto:
-    face_module = onto.FaceModule()
-    face_module_state = onto.State()
-    face_module.has_current = [face_module_state]
-
-    voice_module = onto.VoiceModule()
-    voice_module_state = onto.State()
-    voice_module.has_current = [voice_module_state]
-
-    input_module = onto.InputModule()
-    input_module_state = onto.State()
-    input_module.has_current = [input_module_state]
-
-
     patient = onto.Patient()
-    patient_state = onto.State()
+    user = onto.User()
 
-
-    sync_reasoner(keep_tmp_file = True)
