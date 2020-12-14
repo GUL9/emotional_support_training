d77e48e96e2fb739ca435c3eb0481a97968be3b4
Anton <a_widmark@hotmail.com>
2020-12-14 14:49:59 +0100

    added first swrl rules

--- test.py
+++ test.py
@@ -5,10 +5,18 @@ onto = get_ontology("ontologies/ontology.owl").load()
 with onto:
     # Patient setup
     patient = onto.Patient()
+    patient_state = onto.State()
     facial_expression = onto.Emotion()
+    voice_expression = onto.Emotion()
 
-    patient.has_facial_expression = [onto.Emotion()]
+    patient.has_state.append(patient_state)
+    #patient.has_facial_expression.append(facial_expression)
+
+
+    # User setup
     user = onto.User()
+
+
 
 
 
